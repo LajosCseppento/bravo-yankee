c074d2d10709eebf049c7ee7f77be804ca47d8b5
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-03-18 09:08:49 +0100

    Add package name & version to footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -4,7 +4,8 @@ import React from 'react';
 export default function Footer() {
   return (
     <Typography variant="body2" width="100%" alignItems="center">
-      Built with React, MUI and ❤️
+      {process.env.REACT_APP_PACKAGE_NAME}{' '}
+      {process.env.REACT_APP_PACKAGE_VERSION} - Built with React, MUI and ❤️
     </Typography>
   );
 }
